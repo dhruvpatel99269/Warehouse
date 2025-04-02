f08c351cfceabcdc4745020fed0fb092cc1b1efe
Dhruv Patel <99269dhruvpatel@gmail.com>
2025-04-02 20:50:07 +0530

    error removal

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from routes import register_routes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 app = Flask(__name__)
 CORS(app)
---
+++ flaskapi/services/ppe_email.py
@@ -0,0 +1,37 @@
+import os
+import base64
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
+
+# Your SendGrid API Key
+SENDGRID_API_KEY = "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"
+
+def send_email_alert():
+    message = Mail(
+        from_email="99269dhruvpatel@gmail.com",  # Your verified SendGrid email
+        to_emails="dhruv.patel.btech2022@sitpune.edu.in",    # Replace with the recipient's email
+        subject="🚨 PPE non-compliance!",
+        html_content="<h1>Alert!</h1><p>A ppe non compliance has been detected. See the attached image.</p>"
+    )
+
+    # Read the fall detection image
+    file_path = r"E:\SEM-6 Projects\PBL-Project\flaskapi\alerts\ppe_first_violation.jpg"
+    with open(file_path, "rb") as f:
+        file_data = f.read()
+        encoded_file = base64.b64encode(file_data).decode()  # Encode file in base64
+
+    # Create attachment
+    attachment = Attachment(
+        file_content=FileContent(encoded_file),
+        file_type=FileType("image/jpeg"),
+        file_name=FileName("ppe_alert.jpg"),
+        disposition=Disposition("attachment")
+    )
+    message.attachment = attachment  # Attach the image
+
+    try:
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sg.send(message)
+        print(f"✅ Email sent! Status Code: {response.status_code}")
+    except Exception as e:
+        print(f"❌ Error sending email: {str(e)}")
